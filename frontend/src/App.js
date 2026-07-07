d9e2f16eeeb281c65175826f4bb506ce547a706c
Hang Ruan <46987261+keysmon@users.noreply.github.com>
2026-07-07 12:32:18 -0700

    feat: DefectLens React UI (analyze, search, report export)

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import DefectLens from "./DefectLens";
+import "./App.css"; // Optional global styles
+
+function App() {
+  return (
+    <div className="App">
+      <DefectLens />
+    </div>
+  );
+}
+
+export default App;
